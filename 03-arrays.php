8eb42f00e85cfc8e141b524b09a01b677d5681fb
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-01-31 11:34:35 -0300

    Adiciona Pink Floyd ao array

--- 03-arrays.php
+++ 03-arrays.php
@@ -12,7 +12,7 @@
     <h2>Arrays numéricos/indexados</h2>
 <?php
 // Sintaxe com colchetes
-$bandas = ["Savatage", "Rush", "Slayer"];
+$bandas = ["Savatage", "Rush", "Slayer", "Pink Floyd"];
 
 // Sintaxe com a função array()
 $cursos = array("HTML5", "Node.js", "PHP", "SQL");
