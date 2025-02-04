52b7cfb34666aac57a6f3a263a0d8b94e004fbef
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-04 11:17:15 -0300

    Adiciona propriedades/atributos e acessa informações

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -11,9 +11,26 @@
     <?php
     /* Criando uma instância de uma classe genérica chamada stdClass (Standar Class)*/
     $usuario = new stdClass(); // instância ou objeto  
+
+    //Adicionando propriedades/atributos ao objeto
+    $usuario->nome = "Chapolin Colorado";
+    $usuario->idade = 25;
+    $usuario->email = "chapolin@gmail.com";
+    $usuario->telefones = ["11-2135-0000", "11-98888-7777"];
+
+    echo $usuario->nome;
+    echo "<p><mark>$usuario->nome</mark></p>"; //interpolação
     ?>
 
         <h2>Analisando a estrutura do objeto</h2>
         <pre><?=var_dump($usuario)?></pre>
+        <hr>
+
+        <h2>Saída de Dados</h2>
+        <p>Nome: <?=$usuario->nome?></p>
+        <p>Idade: <?=$usuario->idade?> anos</p>
+        <p>Celular: <?=$usuario->telefones[1]?></p>
+        <hr>
+
 </body>
 </html>
