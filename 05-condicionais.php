ed9b08f62941bae2fca66aa9ed9881cc2513c49c
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-04 11:34:45 -0300

    Cria arquivo exemplos de Condicionais

---
+++ 05-condicionais.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Condicionais</title>
+</head>
+<body>
+    <h1>Estruturas condicionais</h1>
+    <hr>
+    
+    <h2>Simples</h2>
+
+    
+    <hr>
+
+    <h2>Composta</h2>
+
+
+    <hr>
+
+    <h2>Encadeada</h2>
+
+
+</body>
+</html>
