3a26f91ab6be63f429356083ffe967af95299274
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-05 09:34:11 -0300

    Refatora condicional simples

--- 05-condicionais-refatorada.php
+++ 05-condicionais-refatorada.php
@@ -3,26 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Condicionais</title>
+    <title>PHP - Condicionais "REFATORADA"</title>
     <style>
         .alerta-compra{color: red;}
         .normal{color: darkgreen;}
     </style>
 </head>
 <body>
-    <h1>Estruturas condicionais</h1>
+    <h1>(VERSÃO 2) - Estruturas condicionais</h1>
     <hr>
 
     <h2>Simples usando <code>if</code></h2>    
 <?php
 $numero = 5;
-
 if($numero > 1){
-    echo "<p>$numero é maior que 1</p>";
-}
-
-// Sintaxe omitindo as chaves
-if($numero > 1) echo "<p>$numero é maior que 1</p>";
+?>  
+    <p><?=$numero?> é maior que 1</p>
+<?php
+};
 ?>
     <hr>
     <h2>Composta</h2>
