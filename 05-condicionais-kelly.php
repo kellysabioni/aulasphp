3f6ed4a264f1bc9a777a7c68f4351bbd8138fb4d
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-05 09:09:00 -0300

    Inclui condicional composta

---
+++ 05-condicionais-kelly.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Condicionais</title>
+</head>
+<body>
+    <h1>Estruturas condicionais</h1>
+    <hr>
+    
+    <h2>Simples usando <code>if</code></h2>
+<?php
+$numero = 10;
+
+if($numero > 1){
+    echo "<p>$numero é maior que 1</p>";
+}
+
+//Sintaxe omitindo as chaves
+if($numero > 1) echo "<p>$numero é maior que 1</p>";
+?>
+    <hr>
+
+    <h2>Composta</h2>
+
+
+    <hr>
+
+    <h2>Encadeada</h2>
+
+
+</body>
+</html>
--- 05-condicionais.php
+++ 05-condicionais.php
@@ -4,31 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Condicionais</title>
+    <style>
+        .alerta-compra{color: red;}
+        .normal{color: darkgreen;}
+    </style>
 </head>
 <body>
     <h1>Estruturas condicionais</h1>
     <hr>
-    
-    <h2>Simples usando <code>if</code></h2>
+
+    <h2>Simples usando <code>if</code></h2>    
 <?php
-$numero = 10;
+$numero = 5;
 
 if($numero > 1){
     echo "<p>$numero é maior que 1</p>";
 }
 
-//Sintaxe omitindo as chaves
+// Sintaxe omitindo as chaves
 if($numero > 1) echo "<p>$numero é maior que 1</p>";
 ?>
     <hr>
-
     <h2>Composta</h2>
+<?php
+$produto = "Ultrabook Asus";
+$qtdEmEstoque = 0; // o que temos
+$qtdCritica = 5; // mínimo necessário
 
+echo "<h3>$produto</h3>";
+echo "<h4>Quantidade em estoque: $qtdEmEstoque</h4>";
 
+if ($qtdEmEstoque < $qtdCritica) {
+    echo '<p class = "alerta-compra">É necessário comprar!</p>';
+
+    // Condicional ANINHADA
+    if($qtdEmEstoque === 0){
+        echo '<p class = "alerta-compra"><strong> <mark>🚨URGENTE🚨</mark></strong></p>';
+    }
+} else {
+    echo '<p class = "normal" >Estoque normal</p>';
+}
+?>
     <hr>
-
     <h2>Encadeada</h2>
-
 
 </body>
 </html>
