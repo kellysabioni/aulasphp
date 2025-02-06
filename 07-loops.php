1f3bd569207b84ad64ae90684f7ba389ba0c3f30
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-06 09:33:39 -0300

    Cria 07-Loops.php

---
+++ 07-loops.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Loops</title>
+</head>
+<body>
+    <h1>Trabalhando com comandos/laços de repetição</h1>    
+    <hr>
+
+    <h2>WHILE (ENQUANTO)</h2>
+    <p>Executa ações <b>enquanto a condição for verdadeira</b>. Se a condição inicialmente não é verdadeira, o loop não funciona nenhuma vez.</p>
+    <hr>
+
+    <h2>DO/WHILE (parecido com REPITA)</h2>
+    <p>Repete <b>pelo menos uma vez</b> as ações e, caso a condição continue verdadeira, segue fazendo outros ciclos de repetição até a condição se tornar falsa.</p>
+
+    <hr>
+
+    <h2>FOR (PARA)</h2>
+    <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>
+
+
+    <hr>
+
+</body>
+</html>
