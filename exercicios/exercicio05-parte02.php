36b0327064681e5eb5a3cd88d076f3f040f60aec
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-12 10:55:27 -0300

    Exercicio 05 parte 02
    Refatorado
    Use duas funções: uma para isolar a lógica de cálculo da média, e outra para isolar a lógica de verificação da situação do aluno
    Use recursos do CSS (ou Bootstrap) para formatar em vermelho caso reprovado, e azul caso aprovado.

---
+++ exercicios/exercicio05-parte02.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - FUNÇÕES</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container text-center ">
+
+        <h1 class="mb-4">EXERCICIO 05 - FUNÇÕES (parte 02)</h1>
+        <hr>
+        <p>Use duas funções: uma para isolar a lógica de cálculo da média, e outra para isolar a lógica de verificação da situação do aluno.</p>
+        <p>Use recursos do CSS (ou Bootstrap) para formatar em vermelho caso reprovado, e azul caso aprovado.</p>
+        <hr>
+    <?php
+    $aluno = "Kelly";
+  
+    $calcucularMediaDasNotas = function(float $nota1, float $nota2, float $nota3):float {
+
+        return $mediaDasNotas = ($nota1 + $nota2 + $nota3) / 3;
+        
+    };
+
+    $mediaFinal = number_format($calcucularMediaDasNotas(5,5,5), 1,",","." );
+
+    function verificaSituacao ($mediaFinal){
+        return $mediaFinal < 7 ? 
+        '<span class="alert alert-danger">Reprovado</s>' : 
+        '<span class="alert alert-info">Aprovado</span>'; 
+    };
+
+    $situacao = verificaSituacao($mediaFinal);
+?>  
+    <h2>Resultado do Exercício</h2>
+    <p>A média das notas de <?=$aluno?> foi <?=$mediaFinal?>. E está <?=$situacao?>!!! </p>
+    
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
