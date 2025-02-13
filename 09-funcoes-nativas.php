76c4570eedde2cbde9f741b9546e2b93ebb5c190
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-13 08:57:32 -0300

    Atualiza exemplo explode

--- 09-funcoes-nativas.php
+++ 09-funcoes-nativas.php
@@ -36,7 +36,22 @@ $textoSemEspaco = trim($texto);
         
         <p><?=$fraseFeia?></p>
         <p><?=$fraseBunitinha?></p>
-        
+
+        <hr>
+
+        <h3><code>explode</code></h3>
+        <p>Transformar uma string em um array</p>
+
+    <?php
+    $textosLinguagens = "HTML,CSS,JS,PHP,SQL";
+    $arrayLingugens = explode(",", $textosLinguagens);
+    
+    ?>
+    <pre><?=var_dump($textosLinguagens)?></pre>
+    <pre><?=var_dump($arrayLingugens)?></pre>
+    
+    <hr>
+
     </div>    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
