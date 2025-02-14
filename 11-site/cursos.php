e8879284d64a38b04252096903c45b31a2cd74c3
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-14 08:33:36 -0300

    Cria e atualiza página de cursos

---
+++ 11-site/cursos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site com PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Dúvidas</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Página de cursos</h2>
+            <p>Veja os cursos disponíveis</p>
+            <ul>
+                <li>Culinária</li>
+                <li>Direção Defensiva</li>
+                <li>Como escapar de um terremoto</li>
+
+            </ul>
+        </main>
+        
+        <footer>
+            <p>Site criado para o curso Téc. Informática para Internet</p>
+        </footer>
+    </div>    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
