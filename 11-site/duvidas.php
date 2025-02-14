df9e5da9624f359262dbc935bcc522d8fc8d2fdc
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-14 08:37:22 -0300

    Cria e atualiza pagina de duvidas

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -18,14 +18,19 @@
         </header>
 
         <main>
-            <h2>Página de cursos</h2>
-            <p>Veja os cursos disponíveis</p>
-            <ul>
-                <li>Culinária</li>
-                <li>Direção Defensiva</li>
-                <li>Como escapar de um terremoto</li>
+            <h2>Página de dúvidas</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati impedit quam, dicta, laudantium et culpa, molestias sint repellendus quibusdam ex vitae. Maxime mollitia et omnis nulla explicabo? Fuga excepturi sunt beatae adipisci maxime dicta voluptas veritatis provident ex in reiciendis quis nam non explicabo ab, natus eveniet. Id, odio ut?</p>
+            
+            <h3>Principais dúvidas</h3>
 
-            </ul>
+            <details>
+                <summary>Como se cadastrar? </summary>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam corporis cumque dolor, libero necessitatibus quos perspiciatis animi error amet optio?</p>
+            </details>
+            <details>
+                <summary>Os cursos são gratuitos? </summary>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam corporis cumque dolor, libero necessitatibus quos perspiciatis animi error amet optio?</p>
+            </details>
         </main>
         
         <footer>
