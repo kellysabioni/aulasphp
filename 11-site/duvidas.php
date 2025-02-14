da2fc7a9cbdea930319744efed228122b55b6046
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-14 08:59:36 -0300

    Atualiza paginas com modulo footer

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -13,13 +13,5 @@
                 <summary>Os cursos são gratuitos? </summary>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam corporis cumque dolor, libero necessitatibus quos perspiciatis animi error amet optio?</p>
             </details>
-        </main>
-        
-        <footer>
-            <p>Site criado para o curso Téc. Informática para Internet</p>
-        </footer>
-    </div>    
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include "footer.php" ?>
--- 11-site/index.php
+++ 11-site/index.php
@@ -3,13 +3,5 @@
             <h2>Bem-vindo ao site Exemplo</h2>
             <p>Esta é a primeira página do nosso site.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis porro, quaerat, esse pariatur ex culpa asperiores quod ut tenetur quia ea voluptate vitae aliquid rerum officiis, optio quos consequatur perferendis?</p>
-        </main>
-        
-        <footer>
-            <p>Site criado para o curso Téc. Informática para Internet</p>
-        </footer>
-    </div>    
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include "footer.php" ?>
