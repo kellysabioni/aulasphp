ae0aa0a5a6d0f4d5820e2c8ad257a620a292e6c7
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-14 08:53:59 -0300

    Atualiza include com modulo cabecalho

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cursos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <header>
-            <h1>Site com PHP</h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="cursos.php">Cursos</a>
-                <a href="duvidas.php">Dúvidas</a>
-            </nav>
-        </header>
+<?php include "cabecalho.php" ?>
 
-        <main>
             <h2>Página de dúvidas</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati impedit quam, dicta, laudantium et culpa, molestias sint repellendus quibusdam ex vitae. Maxime mollitia et omnis nulla explicabo? Fuga excepturi sunt beatae adipisci maxime dicta voluptas veritatis provident ex in reiciendis quis nam non explicabo ab, natus eveniet. Id, odio ut?</p>
             
--- 11-site/index.php
+++ 11-site/index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <header>
-            <h1>Site com PHP</h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="cursos.php">Cursos</a>
-                <a href="duvidas.php">Dúvidas</a>
-            </nav>
-        </header>
+<?php include "cabecalho.php" ?>
 
-        <main>
             <h2>Bem-vindo ao site Exemplo</h2>
             <p>Esta é a primeira página do nosso site.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis porro, quaerat, esse pariatur ex culpa asperiores quod ut tenetur quia ea voluptate vitae aliquid rerum officiis, optio quos consequatur perferendis?</p>
