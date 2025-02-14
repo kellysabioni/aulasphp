111ec21b046e89ff6cc2643d7ea71b960ddc7023
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-02-14 10:26:55 -0300

    Adiciona switch case para titulo da pagina

--- 11-site/includes/cabecalho.php
+++ 11-site/includes/cabecalho.php
@@ -3,10 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com PHP</title>
+<?php
+// Função para definir o título da página com base na URL
+function obterTituloPagina($caminho) {
+    $titulo = 'Página Desconhecida';
+
+    switch ($caminho) {
+        case '/index.php':
+            $titulo = 'Site com PHP';
+            break;
+        case '/duvidas.php':
+            $titulo = 'Duvidas';
+            break;
+        case '/contatos.php':
+            $titulo = 'Contato';
+            break;
+        case '/cursos.php':
+            $titulo = 'Treinamentos';
+            break;
+        // Adicione mais casos conforme necessário
+        default:
+            $titulo = 'Página Desconhecida';
+            break;
+    }
+
+    return $titulo;
+}
+
+// Obter o caminho da URL da página atual
+$caminho = $_SERVER['REQUEST_URI'];
+
+// Obter o título da página com base na URL
+$tituloPagina = obterTituloPagina($caminho); 
+//echo "Titulo da pagina" $tituloPagina;
+?>
+    <title><?php echo $tituloPagina?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
+
     <div class="container">
         <header>
             <h1>Site com PHP</h1>
